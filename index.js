1ebc926ba1ac05b7c3d4b41f437f128e4df15bab
luis15 <coelho.luisantonio@hotmail.com>
2024-05-23 14:17:28 -0300

    ajustando porta

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get("/", async (req, res) => {
   res.render("index", { title: "Hey", message: "Hello there!" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor iniciado");
 });
